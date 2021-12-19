e9ab015c6963636da65d1404ab68405ee3e2a837
Tanner Coker <tbcoker13@gmail.com>
2021-12-19 16:03:12 -0600

    Time labels will now update every second. It will also exit the program when esc is pressed.

--- SimplePiClock.py
+++ SimplePiClock.py
@@ -9,6 +9,10 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import keyboard
+from datetime import datetime
+import time as tmod
+
 
 #pi screen is 720x576
 
@@ -18,20 +22,23 @@ class Ui_SimplePiClock(object):
 
     def setupUi(self, SimplePiClock):
         SimplePiClock.setObjectName("SimplePiClock")
+
         SimplePiClock.setEnabled(True)
         SimplePiClock.resize(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         SimplePiClock.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.centralwidget = QtWidgets.QWidget(SimplePiClock)
         self.centralwidget.setObjectName("centralwidget")
         self.timeLabel = QtWidgets.QLabel(self.centralwidget)
-        self.timeLabel.setGeometry(QtCore.QRect(self.SCREEN_WIDTH/2 - (421/2), self.SCREEN_HEIGHT/2 - 30, 421, 61))
+
+        self.timeLabel.setGeometry(QtCore.QRect((int)(self.SCREEN_WIDTH/2 - (421/2)), (int)(self.SCREEN_HEIGHT/2 - 30), 421, 61))
         font = QtGui.QFont()
         font.setPointSize(28)
         self.timeLabel.setFont(font)
         self.timeLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.timeLabel.setObjectName("timeLabel")
+
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(self.SCREEN_WIDTH/2 - (421/2), self.SCREEN_HEIGHT/2, 421, 61))
+        self.label.setGeometry(QtCore.QRect((int)(self.SCREEN_WIDTH/2 - (421/2)), (int)(self.SCREEN_HEIGHT/2), 421, 61))
         font = QtGui.QFont()
         font.setPointSize(28)
         self.label.setFont(font)
@@ -39,14 +46,26 @@ class Ui_SimplePiClock(object):
         self.label.setObjectName("label")
         SimplePiClock.setCentralWidget(self.centralwidget)
 
+
         self.retranslateUi(SimplePiClock)
         QtCore.QMetaObject.connectSlotsByName(SimplePiClock)
+
 
     def retranslateUi(self, SimplePiClock):
         _translate = QtCore.QCoreApplication.translate
         SimplePiClock.setWindowTitle(_translate("SimplePiClock", "SimplePiClock"))
-        self.timeLabel.setText(_translate("SimplePiClock", "Time"))
-        self.label.setText(_translate("SimplePiClock", "Date"))
+        
+        if keyboard.is_pressed('esc'):
+            sys.exit(app.exec_())
+
+        curTime = datetime.now()
+
+        time = curTime.strftime("%H:%M:%S")
+        date = curTime.strftime("%m/%d/%y")
+
+        self.timeLabel.setText(_translate("SimplePiClock", time))
+        self.label.setText(_translate("SimplePiClock", date))
+
 
 
 if __name__ == "__main__":
@@ -56,4 +75,12 @@ if __name__ == "__main__":
     ui = Ui_SimplePiClock()
     ui.setupUi(SimplePiClock)
     SimplePiClock.show()
+
+    def update_time():
+        ui.retranslateUi(SimplePiClock)
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update_time)
+    timer.start(1000)
+
     sys.exit(app.exec_())
